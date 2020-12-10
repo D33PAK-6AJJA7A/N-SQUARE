45b7c95f925102be700b5be14ae482324e5a005b
d3ad5h01 <54915730+d3ad5h01@users.noreply.github.com>
2020-12-10 22:57:58 +0530

    Add files via upload

---
+++ N SQUARE/src/boxpuzzle/Theme.java
@@ -0,0 +1,20 @@
+package boxpuzzle;
+import java.awt.Color;
+
+public class Theme {
+	Color colorBack;
+    Color colorFront;
+	Theme(String theme){
+	if(theme=="Dark")
+    {
+		colorBack = new Color(50,50,50);
+    	colorFront = new Color(214,237,23);
+        
+    }
+    else
+    {
+    	colorBack = new Color(51,51,51);
+        colorFront = new Color(250,200,200);          
+    }
+	}
+}
